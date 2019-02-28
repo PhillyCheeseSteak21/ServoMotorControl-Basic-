050c3196a895f87adbd81e9d0f1fcb5212a80805
PhillyCheeseSteak21 <39491310+PhillyCheeseSteak21@users.noreply.github.com>
2019-02-28 09:08:57 -0500

    Add files via upload

---
+++ PhilServomotor.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+Servo myservo;
+int x = analogRead(5);
+
+
+void setup() {
+  myservo.attach(3);
+}
+
+void loop() {
+  x = analogRead(5);
+  x / 180;
+  myservo.write(x);
+  delay(1);
+
+}
